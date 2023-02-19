be93a71a676f5946f80fdbc22dd82cab43ae45c0
mathias <selandermathias00@gmail.com>
2023-02-19 12:59:53 +0100

    move fish git prompt config to only interactive

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,25 @@
-set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
-
-set -g __fish_git_prompt_color_branch blue
-set -g __fish_git_prompt_showupstream "informative"
-set -g __fish_git_prompt_char_upstream_ahead "↑"
-set -g __fish_git_prompt_char_upstream_behind "↓"
-set -g __fish_git_prompt_char_upstream_prefix ""
-
-set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "✚"
-set -g __fish_git_prompt_char_untrackedfiles "…"
-set -g __fish_git_prompt_char_conflictedstate "✖"
-set -g __fish_git_prompt_char_cleanstate "✔"
-
-set -g __fish_git_prompt_color_dirtystate red
-set -g __fish_git_prompt_color_stagedstate yellow
-set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate green
-
 if status is-interactive
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_hide_untrackedfiles 1
+
+    set -g __fish_git_prompt_color_branch blue
+    set -g __fish_git_prompt_showupstream "informative"
+    set -g __fish_git_prompt_char_upstream_ahead "↑"
+    set -g __fish_git_prompt_char_upstream_behind "↓"
+    set -g __fish_git_prompt_char_upstream_prefix ""
+
+    set -g __fish_git_prompt_char_stagedstate "●"
+    set -g __fish_git_prompt_char_dirtystate "✚"
+    set -g __fish_git_prompt_char_untrackedfiles "…"
+    set -g __fish_git_prompt_char_conflictedstate "✖"
+    set -g __fish_git_prompt_char_cleanstate "✔"
+
+    set -g __fish_git_prompt_color_dirtystate red
+    set -g __fish_git_prompt_color_stagedstate yellow
+    set -g __fish_git_prompt_color_invalidstate red
+    set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    set -g __fish_git_prompt_color_cleanstate green
+
     export PATH="$HOME/.cabal/bin:$HOME/.ghcup/bin:$PATH"
     export PATH="$PATH:/home/mathias/.spicetify"
     export PATH="$PATH:/opt/geogebra-5"
