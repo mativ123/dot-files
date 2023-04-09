6676d856ded11b290d5bad26b18155aa950e1012
mathias <selandermathias00@gmail.com>
2023-04-09 16:14:20 +0200

    change mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -11,3 +11,4 @@ vim.keymap.set("n", "<c-t>", builtin.find_files, {noremap = true})
 
 -- floaterm
 vim.keymap.set("n", "<c-cr>", ":FloatermToggle<cr>", {noremap = true})
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {noremap = true})
