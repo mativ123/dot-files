40ba3cc1bbbc48a6186b871ccf19d4e2ae3ed7fb
mathias <selandermathias00@gmail.com>
2023-04-24 14:19:52 +0200

    auto install coc extensions

---
+++ nvim/lua/coc-extensions.lua
@@ -0,0 +1,17 @@
+-- auto install coc extensions
+vim.g.coc_global_extensions = {
+    "coc-prettier",
+    "coc-html",
+    "coc-tsserver",
+    "coc-tslint-plugin",
+    "coc-texlab",
+    "coc-sumneko-lua",
+    "coc-sh",
+    "coc-rust-analyzer",
+    "coc-pyright",
+    "coc-markdownlint",
+    "coc-julia",
+    "coc-json",
+    "coc-css",
+    "coc-clangd",
+}
